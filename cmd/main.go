067266280160d972e1e00123d150dbba1d175914
Tristan Hilbert (aka TFlexSoom) <tchilbert@live.com>
2023-08-29 07:33:00 -0700

    Start golang project

---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+// def lemma compiler entry file for command line processing.
+package main
+
+import (
+	"log"
+)
+
+
+// Entry Function
+func main() {
+	log.Println("Yo Whatup world!")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tflexsoom/deflemma
+
+go 1.21
+
+require github.com/alecthomas/participle/v2 v2.0.0
