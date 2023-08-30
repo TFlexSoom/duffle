7185429c1fa8ed8ddcd3bc8ee011319881ce95fb
Tristan Hilbert (aka TFlexSoom) <tchilbert@live.com>
2023-08-29 18:10:03 -0700

    Inital Attempt at a Grammar

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,26 @@ package main
 
 import (
 	"log"
+	"os"
+
+	"github.com/tflexsoom/deflemma/internal/parsing"
 )
 
 
 // Entry Function
 func main() {
-	log.Println("Yo Whatup world!")
+	log.Println("Beginning Parsing!")
+
+	const file = "./example/example0/helloWorld.lfun"
+	r, err := os.Open(file)
+	if err != nil {
+		log.Fatalf("Error: %v\n", err)
+	}
+
+	_, err = parsing.GetParser().Parse(file, r)
+	if err != nil {
+		log.Fatalf("Error: %v\n", err)
+	}
+
+	log.Println("Done!")
 }
---
+++ internal/parsing/lfunGrammar.go
@@ -0,0 +1,174 @@
+// 
+// author: Tristan Hilbert
+// date: 8/29/2023
+// filename: lfunGrammar.go
+// desc: Parsing Grammar to Build AST for lfun files
+//
+//
+package parsing
+
+import (
+	"github.com/alecthomas/participle/v2"
+	"github.com/alecthomas/participle/v2/lexer"
+)
+
+//// Lexer
+func getLexer() *lexer.StatefulDefinition {
+	return lexer.MustSimple([]lexer.SimpleRule{
+		{"Ident", `[a-zA-Z]+[a-zA-Z\d]+\w*`},
+		{"Whitespace", `[ \t]+`},
+		{"EOL", `[\r]?[\n]`},
+		{"Operator", `[!\d\w]+[\w]*`},
+		{"Backtick", "`"},
+	})
+}
+
+func GetParser() *participle.Parser[Module] {
+	return participle.MustBuild[Module](
+		participle.Lexer(getLexer()),
+		participle.Elide("Whitespace"),
+		participle.UseLookahead(2),
+		participle.Union[Expression](
+			DelayedExpression{},
+			ResolvingExpression{},
+			ReferenceExpression{},
+			AssignmentExpression{},
+			OperatorExpression{},
+		))
+}
+
+
+//// Grammar
+
+type Module struct {
+	Pos lexer.Position
+
+	Imports []*Import `("use" @@ (EOL)+)*`
+	Configs []*Config `(( "->" | ">>" ) @@ (EOL)+)*`
+	Functions []*Function `(@@ (EOL)+)*`
+}
+
+type Import struct {
+	Pos lexer.Position
+
+	ListImport []string `"(" ( @Ident ( "," (EOL)?  @Ident )* )? ")"`
+	SingleImport string `| @Ident`
+}
+
+type Uniqueness bool
+
+func (u *Uniqueness) Capture(values []string) error {
+	*u = values[0] == "->"
+	return nil
+}
+
+type Config struct {
+	Pos lexer.Position
+	IsUnique *Uniqueness `@(">>" | "->")`
+	Input *Input `@@`
+}
+
+type Input struct {
+	Pos lexer.Position
+
+	Type *Type `@@`
+	Name string `@Ident`
+}
+
+type Type struct {
+	Name string `@Ident`
+};
+
+
+type Function struct {
+	Pos lexer.Position
+
+	Annotations []string `("@" @Ident)*`
+	Type *Type `(?= @@ ("{" | ":" | "~"))`
+	Name string `@Ident`
+	Inputs []*Input `("~" @@)*`
+	Expressions []*Expression `("{" (@@ (";" | EOL))* "}")?`
+	Patterns []*Pattern `(":\n" ( @@ EOL)* EOL)?`
+}
+
+type Expression interface { 
+	expression()
+	lexPosition() lexer.Position
+}
+
+func (config Config) expression() {}
+func (config Config) lexPosition() lexer.Position {
+	return config.Pos
+}
+
+type DelayedExpression struct {
+	Pos lexer.Position
+
+	Expression *Expression `Backtick @@ Backtick`
+}
+
+func (delayed DelayedExpression) expression() {}
+func (delayed DelayedExpression) lexPosition() lexer.Position {
+	return delayed.Pos
+}
+
+
+type ResolvingExpression struct {
+	Pos lexer.Position
+
+	Name string `@Ident`
+	Args []*Expression `@@+`
+}
+func (resolving ResolvingExpression) expression() {}
+func (resolving ResolvingExpression) lexPosition() lexer.Position {
+	return resolving.Pos
+}
+
+type ReferenceExpression struct {
+	Pos lexer.Position
+	
+	Name string `@Ident`
+}
+func (reference ReferenceExpression) expression() {}
+func (reference ReferenceExpression) lexPosition() lexer.Position {
+	return reference.Pos
+}
+
+type Shallowness bool
+
+func (isShallow *Shallowness) Capture(values []string) error {
+	*isShallow = values[0] == "<-"
+	return nil
+}
+
+type AssignmentExpression struct {
+	Pos lexer.Position
+
+	Name string `@Ident`
+	IsShallow *Shallowness `("<-" | "<<")`
+	Value *Expression `@@`
+}
+func (assigning AssignmentExpression) expression() {}
+func (assigning AssignmentExpression) lexPosition() lexer.Position {
+	return assigning.Pos
+}
+
+type OperatorExpression struct {
+	Pos lexer.Position
+	
+	Left *Expression `@@`
+	Op string `@Operator`
+	Right *Expression `@@`
+}
+func (operator OperatorExpression) expression() {}
+func (operator OperatorExpression) lexPosition() lexer.Position {
+	return operator.Pos
+}
+
+type Pattern struct {
+	Pos lexer.Position
+
+	Name string `@Ident`
+	Inputs []*Input `@@* "="`
+	Definition *Expression `@@`
+}
