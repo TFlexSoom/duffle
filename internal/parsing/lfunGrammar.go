4b5db71365aaac2cfe6b93cba4033999d2da77ef
Tristan Hilbert (aka TFlexSoom) <tchilbert@live.com>
2023-08-30 09:04:18 -0700

    Fix Parser... Bug: Figure Out Arguments Recursion Exit Condition... maybe we need to use parenthesis

--- internal/parsing/lfunGrammar.go
+++ internal/parsing/lfunGrammar.go
@@ -1,10 +1,7 @@
-// 
 // author: Tristan Hilbert
 // date: 8/29/2023
 // filename: lfunGrammar.go
 // desc: Parsing Grammar to Build AST for lfun files
-//
-//
 package parsing
 
 import (
@@ -12,31 +9,19 @@ import (
 	"github.com/alecthomas/participle/v2/lexer"
 )
 
-//// Lexer
+// // Lexer
 func getLexer() (*lexer.StatefulDefinition, error) {
 	return lexer.NewSimple([]lexer.SimpleRule{
-		{"Ident", `[a-zA-Z]+[a-zA-Z\d]+\w*`},
+		{"IDENTIFIER", `[a-zA-Z]+[a-zA-Z\d]+\w*`},
 		{"WHITESPACE", `[ \t]+`},
-		{"EOL", `\n`},
-		{"OPERATOR", `[!\d\w]+[\w]*`},
+		{"EOL", `(\r)?\n`},
+		// {"OPERATOR", `[^\d\w]+[^\w]*`},
 		{"BACKTICK", "`"},
-		{"USE", "use"},
-		{"@", "@"},
-		{`"("`, "("},
-		{`")"`, ")"},
-		{"->", "->"},
-		{">>", ">>"},
-		{"<-", "<-"},
-		{"<<", "<<"},
-		{"~", "~"},
-		{"{", "{"},
-		{"}", "}"},
-		{",", ","},
-		{";", ";"},
+		{"PUNCTUATION", `[-[!@#$%^&*()+_={}\|:;"'<,>.?/]|]`},
 	})
 }
 
-func GetParser() (*participle.Parser[Module], error)	 {
+func GetParser() (*participle.Parser[Module], error) {
 	var lexer, err = getLexer()
 	if err != nil {
 		return nil, err
@@ -44,71 +29,70 @@ func GetParser() (*participle.Parser[Module], error)	 {
 
 	return participle.Build[Module](
 		participle.Lexer(lexer),
-		participle.Elide("Whitespace"),
+		participle.Elide("WHITESPACE"),
+		// participle.UseLookahead(5),
 		participle.Union[Expression](
-			DelayedExpression{},
+			// DelayedExpression{},
 			ResolvingExpression{},
 			ReferenceExpression{},
-			AssignmentExpression{},
-			OperatorExpression{},
+			// AssignmentExpression{},
+			// OperatorExpression{},
 		))
 }
-
 
 //// Grammar
 
 type Module struct {
 	Pos lexer.Position
 
-	Imports []*Import `(Use @@ EOL+)`
-	Configs []*Config `(@@ EOL+)*`
+	Imports   []*Import   `("use" @@ EOL+)*`
+	Configs   []*Config   `(@@ EOL+)*`
 	Functions []*Function `(@@ EOL+)*`
 }
 
 type Import struct {
 	Pos lexer.Position
 
-	ListImport []string `"(" EOL+ ( @Ident EOL+ ( , EOL+  @Ident )* )? ")"`
-	// SingleImport string `| @Ident`
+	ListImport   []string `"(" ( EOL+ @IDENTIFIER EOL+ ( "," EOL+  @IDENTIFIER )* )? ")"`
+	SingleImport string   `| @IDENTIFIER`
 }
 
 type Uniqueness bool
 
 func (u *Uniqueness) Capture(values []string) error {
-	*u = values[0] == "->"
+	*u = values[0] == "-"
 	return nil
 }
 
 type Config struct {
-	Pos lexer.Position
-	IsUnique *Uniqueness `@( >> | -> )`
-	Input *Input `@@`
+	Pos      lexer.Position
+	IsUnique *Uniqueness `@("-" | ">" ) ">"`
+	Input    *Input      `@@`
 }
 
 type Input struct {
 	Pos lexer.Position
 
-	Type *Type `@@`
-	Name string `@Ident`
+	Type *Type  `@@`
+	Name string `@IDENTIFIER`
 }
 
 type Type struct {
-	Name string `@Ident`
-};
-
+	Name string `@IDENTIFIER`
+}
 
 type Function struct {
 	Pos lexer.Position
 
-	Annotations []string `("@" @Ident)*`
-	Type *Type `(?= @@ ( { | : | ~ ))`
-	Name string `@Ident`
-	Inputs []*Input `(~ @@)*`
-	Expressions []*Expression `( { (@@ (; | EOL))* })?`
-	Patterns []*Pattern `(: EOL ( @@ EOL)* EOL)?`
+	Annotations []string      `("@" @IDENTIFIER)*`
+	Type        *Type         `(@@ (?= IDENTIFIER "{" | ":" | "~" ))?`
+	Name        string        `@IDENTIFIER`
+	Inputs      []*Input      `( "~" @@)*`
+	Expressions []*Expression `( "{" EOL* ( @@ (";" | EOL) EOL* )* "}" )?`
+	Patterns    []*Pattern    `( ":" EOL ( @@ EOL )* EOL )?`
 }
 
-type Expression interface { 
+type Expression interface {
 	expression()
 	lexPosition() lexer.Position
 }
@@ -118,24 +102,24 @@ func (config Config) lexPosition() lexer.Position {
 	return config.Pos
 }
 
-type DelayedExpression struct {
-	Pos lexer.Position
+// type DelayedExpression struct {
+// 	Pos lexer.Position
 
-	Expression *Expression `Backtick @@ Backtick`
-}
+// 	Expression *Expression `BACKTICK @@ BACKTICK`
+// }
 
-func (delayed DelayedExpression) expression() {}
-func (delayed DelayedExpression) lexPosition() lexer.Position {
-	return delayed.Pos
-}
-
+// func (delayed DelayedExpression) expression() {}
+// func (delayed DelayedExpression) lexPosition() lexer.Position {
+// 	return delayed.Pos
+// }
 
 type ResolvingExpression struct {
 	Pos lexer.Position
 
-	Name string `@Ident`
+	Name string        `@IDENTIFIER`
 	Args []*Expression `@@+`
 }
+
 func (resolving ResolvingExpression) expression() {}
 func (resolving ResolvingExpression) lexPosition() lexer.Position {
 	return resolving.Pos
@@ -143,49 +127,52 @@ func (resolving ResolvingExpression) lexPosition() lexer.Position {
 
 type ReferenceExpression struct {
 	Pos lexer.Position
-	
-	Name string `@Ident`
+
+	Name string `@IDENTIFIER`
 }
+
 func (reference ReferenceExpression) expression() {}
 func (reference ReferenceExpression) lexPosition() lexer.Position {
 	return reference.Pos
 }
 
-type Shallowness bool
+// type Shallowness bool
 
-func (isShallow *Shallowness) Capture(values []string) error {
-	*isShallow = values[0] == "<-"
-	return nil
-}
+// func (isShallow *Shallowness) Capture(values []string) error {
+// 	*isShallow = values[0] == "<-"
+// 	return nil
+// }
 
-type AssignmentExpression struct {
-	Pos lexer.Position
+// type AssignmentExpression struct {
+// 	Pos lexer.Position
 
-	Name string `@Ident`
-	IsShallow *Shallowness `@("<-" | "<<")`
-	Value *Expression `@@`
-}
-func (assigning AssignmentExpression) expression() {}
-func (assigning AssignmentExpression) lexPosition() lexer.Position {
-	return assigning.Pos
-}
+// 	Name      string       `@IDENTIFIER`
+// 	IsShallow *Shallowness `@("<-" | "<<")`
+// 	Value     *Expression  `@@`
+// }
 
-type OperatorExpression struct {
-	Pos lexer.Position
-	
-	Left *Expression `@@`
-	Op string `@Operator`
-	Right *Expression `@@`
-}
-func (operator OperatorExpression) expression() {}
-func (operator OperatorExpression) lexPosition() lexer.Position {
-	return operator.Pos
-}
+// func (assigning AssignmentExpression) expression() {}
+// func (assigning AssignmentExpression) lexPosition() lexer.Position {
+// 	return assigning.Pos
+// }
+
+// type OperatorExpression struct {
+// 	Pos lexer.Position
+
+// 	Left  *Expression `@@`
+// 	Op    string      `@IDENTIFIER`
+// 	Right *Expression `@@`
+// }
+
+// func (operator OperatorExpression) expression() {}
+// func (operator OperatorExpression) lexPosition() lexer.Position {
+// 	return operator.Pos
+// }
 
 type Pattern struct {
 	Pos lexer.Position
 
-	Name string `@Ident`
-	Inputs []*Input `@@* "="`
+	Name       string      `@IDENTIFIER`
+	Inputs     []*Input    `@@* "="`
 	Definition *Expression `@@`
 }
