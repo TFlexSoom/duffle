13f33d64ace8f42e3ffa0644dd4b50d402e6771f
Tristan Hilbert (aka TFlexSoom) <tchilbert@live.com>
2023-12-15 12:40:14 -0800

    Decide instead that the parser should output the IR straight out... use better interfaces

--- internal/typing/typing.go
+++ internal/typing/typing.go
@@ -1,18 +1,11 @@
 package typing
 
 import (
-	"github.com/tflexsoom/duffle/internal/intermediate"
 	"github.com/tflexsoom/duffle/internal/parsing/dflgrammar"
 )
 
 const PASS_STRING = "PASS"
 
 func TypeCheck(fileName string, ast dflgrammar.Module) (string, error) {
-	symbols := make(map[uint64]intermediate.SymbolPosition)
-	_, _, err := intermediate.GetIR(fileName, ast, &symbols)
-	if err != nil {
-		return "", err
-	}
-
 	return PASS_STRING, nil
 }
